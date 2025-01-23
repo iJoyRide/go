5cf343123720f445ae2870f80d36d1d54e27837e
iJoyRide <sumfeis3@gmail.com>
2025-01-23 08:52:52 +0800

    Get input from user

--- go_essentials/investment_calculator.go
+++ go_essentials/investment_calculator.go
@@ -6,11 +6,19 @@ import (
 )
 
 func main() {
+	const inflationRate = 6.5
 	var investmentAmount float64 = 1000 //explicitly declare variables as float64
-	var expectedReturnRate = 5.5        // or use .0 for literals
+	expectedReturnRate := 5.5           // or use .0 for literals
 	years := 10.0                       // You used the := shorthand which is concise and idiomatic in Go
 
+	fmt.Print("Enter the investment amount: ")
+
+	fmt.Scan(&investmentAmount)
+	fmt.Scan(&years)
+
 	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
